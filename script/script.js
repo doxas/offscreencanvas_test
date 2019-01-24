0c4f6ea59161def4a28ba992f764c8794a50d7b5
doxas <eizanco@gmail.com>
2019-01-24 17:06:25 +0900

    add button and color

--- script/script.js
+++ script/script.js
@@ -1,31 +1,55 @@
 
 (() => {
-    const CANVAS_SIZE = 512;
+    let isStart = false;
     let canvas = null;
+    let offscreen = null;
     let worker = null;
 
     window.addEventListener('load', () => {
-        let isw = window.Worker != null;
-        if(isw === true){
-            // load worker script
-            worker = new Worker('./script/worker.js');
-            worker.addEventListener('message', (e) => {
-                console.log(e);
-            }, false);
-        }else{
-            console.log('ERR: webworker not supported');
-            return;
-        }
+        let e = document.body.querySelector('#run');
+        e.addEventListener('click', () => {
+            if(isStart === true){
+                isStart = false;
+                e.value = 'run';
+                worker.postMessage({type: 'keydown', key: 'Escape'});
+            }else{
+                isStart = true;
+                e.value = 'stop';
+                initialize();
+            }
+        }, false);
+    }, false);
 
-        canvas = document.querySelector('#canvas');
-        canvas.width  = CANVAS_SIZE;
-        canvas.height = CANVAS_SIZE;
-        let oc = canvas.transferControlToOffscreen();
-        worker.postMessage({type: 'init', offscreen: oc}, [oc]);
+    function initialize(){
+        if(canvas == null){
+            let isw = window.Worker != null;
+            if(isw === true){
+                worker = new Worker('./script/worker.js');
+                worker.addEventListener('message', (e) => {
+                    console.log(e);
+                }, false);
+            }else{
+                console.log('ERR: webworker not supported');
+                return;
+            }
+            canvas = document.body.querySelector('#canvas');
+            offscreen = canvas.transferControlToOffscreen();
+            worker.postMessage({type: 'init', offscreen: offscreen}, [offscreen]);
+            let s = Math.min(window.innerWidth, window.innerHeight);
+            worker.postMessage({type: 'resize', width: s, height: s});
+        }else{
+            let s = Math.min(window.innerWidth, window.innerHeight);
+            worker.postMessage({type: 'run', width: s, height: s});
+        }
 
         window.addEventListener('keydown', (eve) => {
             worker.postMessage({type: 'keydown', key: eve.key});
         }, false);
-    }, false);
+
+        window.addEventListener('resize', (eve) => {
+            let w = Math.min(window.innerWidth, window.innerHeight);
+            worker.postMessage({type: 'resize', width: w, height: w});
+        }, false);
+    };
 })();
 
--- script/worker.js
+++ script/worker.js
@@ -7,11 +7,14 @@ let run = false;
 onmessage = (e) => {
     if(e == null || e.data == null || e.data.hasOwnProperty('type') !== true){
         postMessage({ready: false});
-        return;
     }
     if(e.data.type === 'keydown'){
         run = e.data.key !== 'Escape';
-        return;
+    }
+    if(e.data.type === 'resize'){
+        canvas.width = canvasWidth = e.data.width;
+        canvas.height = canvasHeight = e.data.height;
+        iData = ctx.createImageData(e.data.width, e.data.height);
     }
     if(e.data.type === 'init'){
         if(canvas == null){
@@ -25,14 +28,21 @@ onmessage = (e) => {
             iData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
             startTime = Date.now();
         }
+        render();
     }
-
-    render();
+    if(e.data.type === 'run'){
+        canvas.width = canvasWidth = e.data.width;
+        canvas.height = canvasHeight = e.data.height;
+        iData = ctx.createImageData(e.data.width, e.data.height);
+        run = true;
+        startTime = Date.now();
+        render();
+    }
 };
 
 function render(){
     let t = (((Date.now() - startTime) / 1000.0) % 50.0) / 25.0;
-    let nowTime = Math.sin(t * Math.PI) * 0.5;
+    let nowTime = Math.sin(t * Math.PI) * 0.45 + 0.15;
     for(let column = 0; column < canvasWidth; ++column){
         for(let row = 0; row < canvasHeight; ++row){
             let z = {
@@ -50,11 +60,12 @@ function render(){
                     break;
                 }
             }
-            let color = count / LOOP_COUNT * 255;
+            let color = count / LOOP_COUNT;
+            let hsv = hsva(200 + color * 360, 1.0, 1.0, 1.0);
             let index = (row * canvasWidth + column) * 4;
-            iData.data[index + 0] = color;
-            iData.data[index + 1] = color;
-            iData.data[index + 2] = color;
+            iData.data[index + 0] = hsv[0] * 255 * color * 2.0;
+            iData.data[index + 1] = hsv[1] * 255 * color * 2.0;
+            iData.data[index + 2] = hsv[2] * 255 * color * 2.0;
             iData.data[index + 3] = 255;
         }
     }
@@ -62,5 +73,25 @@ function render(){
     if(run === true){
         requestAnimationFrame(render);
     }
+}
+
+function hsva(h, s, v, a){
+    if(s > 1 || v > 1 || a > 1){return;}
+    let th = h % 360;
+    let i = Math.floor(th / 60);
+    let f = th / 60 - i;
+    let m = v * (1 - s);
+    let n = v * (1 - s * f);
+    let k = v * (1 - s * (1 - f));
+    let color = new Array();
+    if(!s > 0 && !s < 0){
+        color.push(v, v, v, a);
+    } else {
+        let r = new Array(v, n, m, m, k, v);
+        let g = new Array(k, v, v, n, m, m);
+        let b = new Array(m, m, k, v, v, n);
+        color.push(r[i], g[i], b[i], a);
+    }
+    return color;
 }
 
